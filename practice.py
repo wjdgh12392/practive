1a59d8e027ab6e8eaf0cc4861c19083784df5471
Jungho Lee <wjdgh12392@naver.com>
2022-01-11 04:56:42 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for i in range(N):
+    r,s = input().split()
+    new_s = []
+    r = int(r)
+    s = list(s)
+    for j in s:
+        for k in range(0,r):
+            new_s.append(j)
+    print(''.join(new_s))
